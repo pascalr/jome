d897884903edd756ee9efc41eee7b0f37c0d9df3
Pascal Rainville <pascal.rainville@gmail.com>
2023-10-15 21:02:29 -0400

    .

--- src/compiler.js
+++ src/compiler.js
@@ -929,6 +929,7 @@ const PROCESSES = {
   "constant.numeric.float.jome": compileAsIs,
   "comment.line.double-slash.jome": () => '',
   "comment.block.jome": () => '',
+  "comment.block.documentation.jome": () => '',
   "string.quoted.backtick.verbatim.jome": (node) => `\`${compileRaw(node.children.slice(1,-1))}\``,
   "string.quoted.double.verbatim.jome": (node) => `"${compileRaw(node.children.slice(1,-1))}"`,
   "string.quoted.single.jome": (node) => `'${compileRaw(node.children.slice(1,-1))}'`,
