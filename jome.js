d43bc8be9f1ae69954d2bf2a8a74a405535c3ddf
Pascal Rainville <pascal.rainville@gmail.com>
2023-11-11 09:57:37 -0500

    .

--- jome.js
+++ jome.js
@@ -1,7 +1,70 @@
 //#!/usr/bin/env node
 
+// Jome executable
+
+// Description:
+// Executes code written in Jome language by compiling it and passing it to node.js executable
+//
+//
+//
+
+// Usage:
+// jome # Executes index.jome in the current directory
+// jome someFile.jome # Executes someFile.jome
+// jome server # Executes index.jome and pass the string server to it.
+//
+//
+//
+
+// Args: (any arguments must be at the beginning of the command!)
+// -c "fileToCompile.jome": Compiles the given file to a .js file.
+// -e "log('Hello')": Execute some code
+// -h: Config mode
+//
+//
+
+// For anything about Jome command itself, using -h
+// It launches an interactive mode.
+// jome -h
+// Jome version 0.0.1
+//
+// Commands available:
+// u: update
+// x: ...
+// x: ...
+// x: ...
+//
+// What do you want to do? (q or Ctrl^C to exit)
+// > ___
+
 // Code to run another file in jome.
 // ./node jome.js file.jome
+
+// TODO: use:
+//
+// const { spawn } = require('child_process');
+// 
+// const someCode = "console.log('hello')";
+// 
+// // Split the code into an array of arguments
+// const codeArguments = ['-e', someCode];
+// 
+// // Spawn a new process with the node command and code arguments
+// const nodeProcess = spawn('node', codeArguments);
+// 
+// // Listen for data on stdout and stderr
+// nodeProcess.stdout.on('data', (data) => {
+//   console.log(`stdout: ${data}`);
+// });
+// 
+// nodeProcess.stderr.on('data', (data) => {
+//   console.error(`stderr: ${data}`);
+// });
+// 
+// // Listen for the process to exit
+// nodeProcess.on('close', (code) => {
+//   console.log(`Child process exited with code ${code}`);
+// });
 
 import { buildAndRunFile } from './src/builder.js';
 import path from 'path';
