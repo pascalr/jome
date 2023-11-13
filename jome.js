6819e96e762ed1b53e01768050522fee331ef059
Pascal Rainville <pascal.rainville@gmail.com>
2023-11-12 19:18:19 -0500

    .

--- jome.js
+++ jome.js
@@ -75,11 +75,6 @@ const __dirname = path.dirname(__filename);
 
 const args = process.argv.slice(2); // Exclude the first two arguments (node executable and script file)
 
-if (args.length === 0) {
-    console.error('Please provide a filename.');
-    process.exit(1);
-}
-
-const fileName = args[0];
+const fileName = (args.length === 0) ? 'index.jome' : args[0];
 const fullPath = path.join(__dirname, fileName)
 buildAndRunFile(fullPath)
