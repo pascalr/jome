c0f63404dbc47612255f021ee577a61e2961e76e
Pascal Rainville <pascal.rainville@gmail.com>
2023-11-19 10:00:17 -0500

    .

--- src/builder.js
+++ src/builder.js
@@ -32,88 +32,6 @@ function saveFile(name, content) {
   });
 }
 
-function buildFile(fullPath, dependencies = [], run=false) {
-  throw new Error('buildFile function is deprecated. Use JomeBuilder')
-  if (!fullPath.endsWith('.jome')) {
-    console.warn('Cannot build file without .jome extension', fullPath);
-    return;
-  }
-
-  console.log('Building File', fullPath);
-
-  try {
-    // Check if the file exists
-    fs.accessSync(fullPath, fs.constants.F_OK);
-  } catch (err) {
-    console.error(`File '${fullPath}' does not exist.`);
-    return null
-  }
-
-  // Read the contents of the file synchronously
-  const data = fs.readFileSync(fullPath, 'utf8');
-
-  let ctx = new CompileContext(run ? {} : {module: true})
-  ctx.currentFile = fullPath // For import relative paths
-  ctx.rootDir = __dirname.slice(0, -3) // FIXME
-
-  let { result, context } = compileGetContext(data, ctx);
-
-  // Handle dependencies relative to the path of the file
-  const directoryPart = path.dirname(fullPath);
-  let missings = []
-  context.dependencies.forEach(dependency => {
-    if (!(dependency in dependencies)) {
-      dependencies.push(dependency)
-      missings.push(dependency)
-    }
-  })
-
-  missings.forEach(missing => {
-    let out = buildFile(path.join(directoryPart, missing), dependencies)
-    let css = out?.context?.stylesheets||{}
-    Object.keys(css).forEach(key => {
-      // FIXME: Don't import multiple times the same css script...
-      context.stylesheets[key] = (context.stylesheets[key]||'') + css[key]
-    })
-  })
-
-  // Modify 'result' as needed
-  if (context.useESM) {
-    result = `import ${JOME_LIB} from 'jome'\n\n` + result;
-  } else {
-    result = `const ${JOME_LIB} = require('jome')\n\n` + result;
-  }
-
-  // Generate the build file name
-  const buildFileName = fullPath.replace(/\.jome$/, '.built.js');
-
-  try {
-    // Write the result to the file synchronously
-    fs.writeFileSync(buildFileName, result);
-
-    if (run) {
-      Object.keys(context.stylesheets).forEach(name => {
-        let cssPath;
-        if (name === '__main__') {
-          cssPath = fullPath.replace(/\.jome$/, '.built.css');
-        } else {
-          cssPath = path.join(path.dirname(fullPath), name)
-        }
-        // TODO: Insert a comment into the stylesheet that says what the source file is
-        fs.writeFileSync(cssPath, context.stylesheets[name]);
-        console.log(`Successfully wrote to '${cssPath}'.`);
-      })
-    }
-
-    console.log(`Successfully wrote to '${buildFileName}'.`);
-  } catch (err) {
-    console.error('Error writing to the file:', err);
-    return null
-  }
-
-  return {buildFileName, context}
-}
-
 /**
  * Process Jome code.
  * 
@@ -327,20 +245,6 @@ class JomeBuilder {
   }
 }
 
-// TODO: Sort out the mess of this file...
-// Use a single buildFile method
-// Use a compile file, because when executing a file I don't want to write to an intermediary file.
-
-async function buildAndRunFile(fullPath, dependencies=[]) {
-  let buildFileName = buildFile(fullPath, dependencies, true)?.buildFileName
-  if (buildFileName) {
-    console.log('Running built')
-    await import(buildFileName);
-  }
-}
-
 module.exports = {
-  buildFile,
-  buildAndRunFile,
   JomeBuilder
 }
