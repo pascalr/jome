a2e2a819ff37eff5445d79355a04fee76da2718a
Pascal Rainville <pascal.rainville@gmail.com>
2023-12-03 08:30:52 -0500

    .

--- jome/lib/exec_sh.js
+++ jome/lib/exec_sh.js
@@ -1,6 +1,7 @@
 const {spawnSync} = require("child_process");
 
 function execSh(cmd) {
+  // FIXME: This does not work I cannot pass strings. Why was I not using execSync instead of spawnSync already? I had trouble with IO I think...
   let s = cmd.split(' ')
   let cmdName = s[0]
   let args = s.slice(1)
--- tmp/sum.test.js
+++ tmp/sum.test.js
@@ -1,8 +1,8 @@
 const jome = require('jome')
 
 
-const {sum} = require("./sum.built.js");
+const sum = require("./sum.built.js");
 
 test('adds 1 + 2 to equal 3', () => {
-  return expect(sum({}, 1, 2)).toBe(3);
+  return expect(sum(1, 2)).toBe(3);
 });
