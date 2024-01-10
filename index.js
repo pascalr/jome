5dcec4c3a855bf49c7089bf6d27f8ae7bfd8b949
Pascal Rainville <pascal.rainville@gmail.com>
2024-01-10 17:06:20 -0500

    .

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const jome = require('jome')
-
-
-global.jome_argv = ["jome", ...process.argv.slice(process.argv.indexOf("--") + 1)];
-
-const execSh = require("jome/lib/exec_sh");
-
-var cmd = global.jome_argv[1]
-if (cmd === 'test') {
-  execSh(`jome_old -- test.jome`);
-} else if (cmd === 's' || cmd === 'server') {
-  execSh(`jome_old -- website/server.jome`);
-} else {
-  console.log(`Jome App v0.0.0.0.1
+const execSh = require("jome-lib/execSh");
+const run_server = require("./website-v2/server.js");
+module.exports = () => {
+  let cmd = process.argv[1];
+  if (cmd === "test") {
+    execSh(`jome test.jome`);
+  } else if (cmd === "s" || cmd === "server") {
+    run_server();
+  } else {
+    console.log(`Jome App v0.0.0.0.1
 
 Usage:
 jome # get this help message
 jome s # start server (or jome server)
-jome test # launch the tests
-`)
-}
+jome test # launch the tests`);
+  }
+};
