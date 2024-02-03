6d6b00769b2f108c8c0db9673c9d83a44ed200d4
Pascal Rainville <pascal.rainville@gmail.com>
2024-02-03 15:26:02 -0500

    .

--- website/server.js
+++ website/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 module.exports = ({ port = 3000 }) => {
   (() => {
     let __chain = express();
-    __chain.use("/jome", express.static("docs")),
+    __chain.use("/jome", express.static("../docs")),
       __chain.get("/", (req, res) => res.redirect("/jome"));
     return __chain.listen(port, function () {
       console.log(`Server listening on port ${port}`);
