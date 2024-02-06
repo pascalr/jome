9eecae55d6dbb98535aeb1af57d86447380f843e
Pascal Rainville <pascal.rainville@gmail.com>
2024-02-05 20:00:45 -0500

    .

--- jome/test/generic.test.js
+++
@@ -1,8 +0,0 @@
-const { compile } = require("../src/compiler.js");
-module.exports = () => {
-  describe("Tests written in jome", function () {
-    test("1 + 1", function () {
-      expect(compile("1 + 1")).toMatch(/1 \+ 1/);
-    });
-  });
-};
