a5bb0892a8b40684d970537784da186fd76ec68a
Pascal Rainville <pascal.rainville@gmail.com>
2024-02-10 09:47:03 -0500

    .

--- jome/src/config_parser.js
+++ jome/src/config_parser.js
@@ -1,16 +1,26 @@
 // Handles config.jome
 
 const {compileFileGetCtx} = require('./compiler')
+const {LexicalEnvironment} = require('./context')
 
 class JomeConfig {
   constructor() {
-    this.globals = [] // {name: "...", source: {...}}
+    this.lexEnv = new LexicalEnvironment()
   }
 }
 
 function parseConfig(absPath) {
   let {result, ctxFile} = compileFileGetCtx(absPath)
+  let lexEnv = ctxFile.lexEnv
+  let configReturn = eval(result)()
   let conf = new JomeConfig()
+  Object.keys(configReturn.utils||{}).forEach(util => {
+    let binding = lexEnv.bindings[util]
+    if (!binding) {
+      throw new Error("Internal Error parsing config.jome, missing binding for "+util)
+    }
+    conf.lexEnv.addBinding(util, binding)
+  })
   return conf
 }
 
