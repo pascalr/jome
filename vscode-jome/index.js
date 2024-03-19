bdee88b733dafdcde6615fb2b9c961ceaea14513
Pascal Rainville <pascal.rainville@gmail.com>
2024-03-19 18:01:23 -0400

    .

--- vscode-jome/index.js
+++ vscode-jome/index.js
@@ -1,4 +1,4 @@
-const {workspace, commands, window} = require("vscode");
+const {workspace, commands, window, ViewColumn} = require("vscode");
 const {LanguageClient} = require("vscode-languageclient/node");
 const path = require("path");
 
@@ -7,6 +7,22 @@ const NodeTreeProvider = require("./src/NodeTreeProvider.js")
 let client;
 
 function activate(context) {
+
+  context.subscriptions.push(
+    commands.registerCommand('jomeEditor.start', () => {
+      // Create and show a new webview
+      const panel = window.createWebviewPanel(
+        'jomeEditor', // Identifies the type of the webview. Used internally
+        'Jome Editor', // Title of the panel displayed to the user
+        ViewColumn.One, // Editor column to show the new webview panel in.
+        {} // Webview options. More on these later.
+      );
+
+      // And set its HTML content
+      panel.webview.html = getWebviewContent();
+    })
+  )
+
     // // The server is implemented in node
     // const serverModule = context.asAbsolutePath(
     // 	path.join('server', 'out', 'server.js')
@@ -70,6 +86,28 @@ commands.registerCommand('extension.vscode-jome.getProgramName', config => {
 window.createTreeView('jomeExplorerNodes', {
   treeDataProvider: new NodeTreeProvider()
 });
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jome editor</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+</body>
+</html>`;
+}
+
+// // Create and show a new webview
+// window.createWebviewPanel(
+//   'jomeEditor', // Identifies the type of the webview. Used internally
+//   'Jome Editor', // Title of the panel displayed to the user
+//   ViewColumn.One, // Editor column to show the new webview panel in.
+//   {} // Webview options. More on these later.
+// );
 
 module.exports = {
   activate, deactivate
