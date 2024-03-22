327d2d27acd9fc03a68905e76b340cdfe08ff359
Pascal Rainville <pascal.rainville@gmail.com>
2024-03-22 18:45:39 -0400

    .

---
+++ vscode-jome/src/JomeNotebookSerializer.js
@@ -0,0 +1,54 @@
+const vscode = require('vscode');
+import { TextDecoder, TextEncoder } from 'util';
+
+class JomeNotebookSerializer {
+  constructor() {
+    this.label = 'Jome Notebook Serializer';
+  }
+
+	async deserializeNotebook(data, token) {
+		const contents = new TextDecoder().decode(data); // convert to String
+
+		// // Read file contents
+		// let raw;
+		// try {
+		// 	raw = JSON.parse(contents);
+		// } catch {
+		// 	raw = { cells: [] };
+		// }
+
+		// // Create array of Notebook cells for the VS Code API from file contents
+		// const cells = raw.cells.map(item => new vscode.NotebookCellData(
+		// 	item.kind,
+		// 	item.value,
+		// 	item.language
+		// ));
+
+    const cells = [new vscode.NotebookCellData(
+			vscode.NotebookCellKind.Code,
+			contents,
+			'jome'// item.cell_type === 'code' ? 'python' : 'markdown'
+		)];
+
+		return new vscode.NotebookData(cells);
+	}
+
+	async serializeNotebook(data, token) {
+    let str = data.cells[0].value
+    return new TextEncoder().encode(str);
+		// // Map the Notebook data into the format we want to save the Notebook data as
+		// const contents = { cells: [] };
+
+		// for (const cell of data.cells) {
+		// 	contents.cells.push({
+		// 		kind: cell.kind,
+		// 		language: cell.languageId,
+		// 		value: cell.value
+		// 	});
+		// }
+
+		// return new TextEncoder().encode(JSON.stringify(contents));
+	}
+}
+
+module.exports = JomeNotebookSerializer
