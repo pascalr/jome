7822afab480c7d52cfe046a25ed0f112d6d32864
Pascal Rainville <pascal.rainville@gmail.com>
2024-07-07 17:55:46 -0400

    .

--- virtual/src/editor.js
+++ virtual/src/editor.js
@@ -1,8 +1,27 @@
+import {Parser} from "acorn"
+import escodegen from "escodegen"
+
 import sample01 from '../samples/torque_calculator.js.txt'
 
 document.addEventListener('DOMContentLoaded', function() {
   let src = sample01
+  let ast = jsToAst(src)
+  escodegen.attachComments(ast.root, ast.comments, ast.tokens);
+  let str = escodegen.generate(ast.root, {comment: true})
+  console.log(ast)
+  console.log(str)
   let highlighted = hljs.highlight(src, {language: 'js'}).value
   document.getElementById('output-editor').innerHTML = highlighted
-  console.log(sample01)
 });
+
+// Converts js code to Asbstract Syntax Tree
+function jsToAst(js) {
+  let comments = [], tokens = [];
+  let root = Parser.parse(js, {
+    ecmaVersion: 6,
+    ranges: true,
+    onComment: comments,
+    onToken: tokens
+  })
+  return {root, comments, tokens}
+}
