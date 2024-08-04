21258fba33d219ef04cef75711a2da6384482a7f
Pascal Rainville <pascal.rainville@gmail.com>
2024-08-04 16:51:56 -0400

    .

--- virtual/docs/samples/torque_calculator.js
+++ virtual/docs/samples/torque_calculator.js
@@ -2,12 +2,23 @@
 
 /*~ # Torque Calculator Example */
 
+//begin input(name: "force", unit: "N*", type: "number", defaultValue: "10", comment: "Newtons or equivalent", onSave: "setValue"}
 //~begin input {"name": "force", "unit": "N*", "type": "number", "defaultValue": "10", "comment": "Newtons or equivalent", "onSave": "setValue"}
   let force = 10;
 //~end
 //~begin input {"name": "distance", "unit": "m*", "type": "number", "defaultValue": "2", "comment": "meters or equivalent", "onSave": "setValue"}
   let distance = 2;
 //~end
+
+/* svg {
+  <!-- Draw the rectangle (beam) -->
+  <rect x="50" y="50" width="200" height="20" fill="steelblue" />
+  <!-- Draw the triangle (pivot point) -->
+  <polygon points="150,80 140,100 160,100" fill="gray" />
+  <!-- Draw the vertical red arrow pointing downward -->
+  <line x1="30" y1="50" x2="30" y2="80" stroke="red" stroke-width="2" />
+  <polygon points="30,80 25,70 35,70" fill="red" />
+}*/
 
 /*~svg:
   <!-- Draw the rectangle (beam) -->
@@ -27,6 +38,6 @@ The unit checker can infer that this block returns a value
 with N*m or equivalent as a unit and shows it.
 */
 
-//~begin code {"startRun": "1"}
+//~begin code(startRun: 1)
 let torque = force * distance;
 //~end
