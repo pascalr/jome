7ac65fd9770afda149c4ad5ab58fb4f7a7911b55
Pascal Rainville <pascal.rainville@gmail.com>
2024-08-08 17:26:46 -0400

    .

--- editor/dev.js
+++ editor/dev.js
@@ -47,6 +47,12 @@ app.use("/self", express.static("."))
 
 app.get("/", (req, res) => res.redirect("/editor"));
 
+app.get("/files", async (req, res) => {
+  fs.readdir(".", { recursive: true }, (err, files) => {
+    res.send(files)
+  });
+});
+
 app.listen(port, function () {
   console.log(`Server listening on port ${port}`);
 })
