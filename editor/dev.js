50873fdee26f863d2a833a5e0b9dfd87c848837a
Pascal Rainville <pascal.rainville@gmail.com>
2024-08-09 17:39:13 -0400

    .

--- editor/dev.js
+++ editor/dev.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const express = require("express");
 
 const port = 3000
@@ -7,7 +8,23 @@ const app = express()
 app.use("/editor", express.static("./docs"))
 app.use("/self", express.static("."))
 
-app.get("/", (req, res) => res.redirect("/editor"));
+app.get("/", (req, res) => res.redirect("/f/README.md"));
+
+app.get(/\/f\/.*/, (req, res) => {
+  let p = path.resolve(path.normalize(req.url.slice(3)))
+  if (!p.startsWith(__dirname)) {
+    res.forbidden();
+  } else {
+    fs.readFile(p, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(400).end();
+        return;
+      }
+      res.send(data)
+    });
+  }
+});
 
 app.get("/files", async (req, res) => {
   fs.readdir(".", { recursive: true }, (err, files) => {
