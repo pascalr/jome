2ef11b0509a4f48bc082c34601f3ea52fdc110e6
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-02 08:28:41 -0400

    .

--- editor/src/canvas_renderer.js
+++ editor/src/canvas_renderer.js
@@ -1,10 +1,12 @@
 import { e } from "./helpers";
 import { validateColor } from "./validate";
 
-export function renderCanvas({name, args}) {
-  let style = ""
-  if (args["background-color"]) {
-    style += "background-color: " + validateColor(args["background-color"])
+export const CanvasRenderer = {
+  render({name, args}) {
+    let style = ""
+    if (args["background-color"]) {
+      style += "background-color: " + validateColor(args["background-color"])
+    }
+    return e('canvas', {width: args.width, height: args.height, style: style || undefined})
   }
-  return e('canvas', {width: args.width, height: args.height, style: style || undefined})
 }
--- editor/src/jome_renderer.js
+++ editor/src/jome_renderer.js
@@ -1,35 +1,50 @@
-import { renderCanvas } from "./canvas_renderer";
+import { CanvasRenderer } from "./canvas_renderer";
 import { e } from "./helpers";
 import { capitalize } from "./utils";
 
 let RENDERERS = {
 
-  input({name, args}) {
+  input: {
+    // Or maybe simply sanitize HTML? Remove < and > ?
+    // No because not just html. For example, it could be CSS style...
+    // I need to sanitize CSS too...
+    // Sanitize the end result? The element added?
+    // It's more work, but it's better whitelisting args like this.
+    // And it's good because the editor will use this  
+    args: {
+      id: "text",
+      type: `enum("text"|"number")`,
+      value: "text",
+    },
+    render({name, args}) {
 
-    let el = e('div', {}, [
-      e('input', {id: args.id, type: args.type||'text', value: args.defaultValue})
-    ])
-    if (args.id) {
-      el.prepend(e('label', {for: args.id}, [capitalize(args.id)+': ']))
-    }
-    if (args.enableEquivalentUnits) {
-      // TODO
-    }
-    if (args.unit) {
-      el.appendChild(e('span', {}, [' '+args.unit]))
-    }
-    if (args.comment) {
-      el.appendChild(e('span', {style: "font-size: 0.8em; color: gray; margin-left: 1.5em;"}, [' '+args.comment]))
-    }
+      let el = e('div', {}, [
+        e('input', {id: args.id, type: args.type||'text', value: args.defaultValue})
+      ])
+      if (args.id) {
+        el.prepend(e('label', {for: args.id}, [capitalize(args.id)+': ']))
+      }
+      if (args.enableEquivalentUnits) {
+        // TODO
+      }
+      if (args.unit) {
+        el.appendChild(e('span', {}, [' '+args.unit]))
+      }
+      if (args.comment) {
+        el.appendChild(e('span', {style: "font-size: 0.8em; color: gray; margin-left: 1.5em;"}, [' '+args.comment]))
+      }
 
-    return el;
+      return el;
+    },
   },
 
-  img({name, args}) {
-    return e('img', {src: args.src})
+  img: {
+    render({name, args}) {
+      return e('img', {src: args.src})
+    },
   },
 
-  canvas: renderCanvas
+  canvas: CanvasRenderer
 
 }
 
@@ -38,7 +53,7 @@ export function renderCommand(ref, cmd) {
   let renderer = RENDERERS[cmd.name]
 
   if (renderer) {
-    let el = renderer(cmd)
+    let el = renderer.render(cmd)
     ref.appendChild(el)
   } else {
     console.error('Missing renderer for: ', cmd.name)
