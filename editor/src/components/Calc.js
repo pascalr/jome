c1b38571a6a5628f4c73baf87eea301f615666e7
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-02 15:55:31 -0400

    .

---
+++ editor/src/components/Calc.js
@@ -0,0 +1,31 @@
+import { e } from "../helpers";
+import { capitalize } from "../utils";
+
+export class Calc extends HTMLElement {
+
+  static get observedAttributes() {
+    return ['name', 'formula', 'comment'];
+  }
+
+  attributeChangedCallback(property, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this[ property ] = newValue;
+  }
+  
+  connectedCallback() {
+
+    let field = e('input', {id: this.name || undefined, type: this.type||'text', value: 'TODO'})
+    field.setAttribute('readonly', true)
+
+    let el = e('div', {}, [field])
+    if (this.name) {
+      el.prepend(e('label', {for: this.name}, [capitalize(this.name)+': ']))
+    }
+    if (this.comment) {
+      el.appendChild(e('span', {style: "font-size: 0.8em; color: gray; margin-left: 1.5em;"}, [' '+this.comment]))
+    }
+
+    this.replaceChildren(el)
+  }
+
+}
--- editor/src/light_editor.js
+++ editor/src/light_editor.js
@@ -3,10 +3,12 @@ import { NeutralinoApp } from './neutralino_app'
 import { HelloWorld } from './components/HelloWorld';
 import { Drawing } from './components/Drawing';
 import { Field } from './components/Field';
+import { Calc } from './components/Calc';
 
 customElements.define("jome-hello-world", HelloWorld)
 customElements.define("jome-drawing", Drawing)
 customElements.define("jome-field", Field)
+customElements.define("jome-calc", Calc)
 
 document.addEventListener('DOMContentLoaded', async () => {
 
