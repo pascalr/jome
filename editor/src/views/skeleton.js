fca9db0e6b553fbfe90bf0065a97a89ec879aca1
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-08 20:01:39 -0400

    .

--- editor/src/views/skeleton.js
+++ editor/src/views/skeleton.js
@@ -10,16 +10,30 @@ export const REF = {
   EDITOR: "editor",
   WINDOW_BAR: "window_bar",
   WINDOW_CONTENT: "window_content",
+  DOCK_BUTTONS: "dock_buttons",
+  DOCK_CONTENT: "dock_buttons",
+  FILE_TABS: "file_tabs",
 }
 
 export function createSkeleton() {
   return e("div", {id: REF.WINDOW, style: "width: 100%; height: 100%; display: flex; flex-direction: column;"}, [
     e("div", {id: REF.WINDOW_BAR}),
     e("div", {id: REF.WINDOW_CONTENT, style: "flex-grow: 1;"}, [
-      e("div", {id: REF.HOME, style: "height: 100%;"}, [
-        e("h1", {id: "tmp_palceholder_home"}, ["Home"])
-      ]),
+      e("div", {id: REF.HOME, style: "height: 100%;"}),
       e("div", {id: REF.EDITOR, style: "height: 100%;"}, [
+        e('div', {className: "split-content"}, [
+          e('div', {id: 'split-0', className: "context_panel"}, [
+            e('div', {id: REF.DOCK_BUTTONS, className: "context_buttons"}),
+            e('div', {id: REF.DOCK_CONTENT, className: "context_content"}),
+          ]),
+          e('div', {id: 'split-1', className: "main_panel"}, [
+            e('div', {id: REF.FILE_TABS, className: "tab-buttons"}),
+            e('div', {id: "editor_content"}), // FIXME
+            e('div', {id: "codemirror_editor"}), // FIXME
+            e('div', {id: "prosemirror_editor"}) // FIXME
+          ]),
+          e('div', {id: 'split-2', className: "selection_panel"})
+        ]),
         e("h1", {id: "tmp_palceholder_editor"}, ["Editor"])
       ])
     ])
