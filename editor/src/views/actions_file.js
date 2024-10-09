b0a7ca2f497dd2715fb614cbba46a5366e8209c9
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-09 11:18:13 -0400

    .

--- editor/src/views/actions_file.js
+++ editor/src/views/actions_file.js
@@ -4,12 +4,17 @@ import iconCopy from '../../assets/icons/copy.svg'
 import iconScissors from '../../assets/icons/scissors.svg'
 import iconClipboard from '../../assets/icons/clipboard.svg'
 import { ActionView } from '../view'
+import { getFilenameFromPath } from '../utils'
 
 class ActionsFile extends ActionView {
 
   render() {
+    let filepath = this.app.getData("CURRENT_FILEPATH")
+    if (!filepath) {return this.getRef().replaceChildren()}
+
+    let filename = getFilenameFromPath(filepath)
     this.getRef().replaceChildren(e('div', {}, [
-      e('div', {className: "panel-main-header"}, ["File • FIXME"]),
+      e('div', {className: "panel-main-header"}, [`File • ${filename}`]),
       createToolSection("Modify", [
         [
           toolIcon(iconCopy, "FIXME"), // FIXME: What is this supposed to do?
--- editor/src/views/actions_project.js
+++ editor/src/views/actions_project.js
@@ -1,18 +1,22 @@
 import {e} from '../helpers'
+import { getFilenameFromPath } from '../utils'
 
 import { ActionView } from '../view'
 
 class ActionsProject extends ActionView {
 
   render() {
+    let path = this.app.getData("PROJECT_PATH")
+    if (!path) {return this.getRef().replaceChildren()}
     
+    let name = getFilenameFromPath(path)
     this.getRef().replaceChildren(e('div', {}, [
-      e('div', {className: "panel-main-header"}, ["Project • FIXME"]),
+      e('div', {className: "panel-main-header"}, [`Project • ${name}`]),
       e('p', {}, ["TODO"])
     ]))
   }
 
-  onProjectChange() { console.log('here!!!'); this.render() }
+  onProjectChange() { this.render() }
 
 }
 
