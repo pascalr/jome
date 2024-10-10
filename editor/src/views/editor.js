c9a1b5b23dfc13dbcfe629e7e6871772750e3151
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-10 08:24:04 -0400

    .

--- editor/src/views/editor.js
+++ editor/src/views/editor.js
@@ -7,21 +7,12 @@ import { createCodemirrorEditor } from "../codemirror/codemirror_editor"
 
 class EditorView extends View {
 
-  onFileChange({filepath, content}) {
-    // update the main source view
-    let doc = new JomeDocument(filepath, content)
-    // let parts = parse(doc) // FIXME: Make this clear that this modifies doc. Refactor
-    // console.log("parts", parts)
-    // document.getElementById('output-editor').innerHTML = renderOutputCode(doc, parts)
-    // document.getElementById('notebook-editor').innerHTML = renderNotebookView(doc, parts)
-
-    let parser = new JomeParser()
-    let segments = parser.parse(doc)
+  onDocumentChange({doc}) {
 
     let ref = getRef(REF.EDITOR_CONTENT)
     ref.replaceChildren()
 
-    segments.forEach(segment => {
+    doc.segments.forEach(segment => {
       if (segment.isRaw) {
         if (doc.extension === 'md') {
           let el = document.createElement("div")
@@ -34,10 +25,6 @@ class EditorView extends View {
         let el = document.createElement("div")
         el.innerHTML = segment.str
         ref.appendChild(el)
-        // ;(segment.commands||[]).forEach(cmd => {
-        //   renderCommand(contentRef, cmd)
-        // })
-        // loadFileProseMirrorEditor(contentRef, doc)
       }
     })
   }
