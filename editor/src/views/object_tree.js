4436659ee3efe5f430cbf15bcfe140dca8c0651c
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-16 08:35:32 -0400

    .

--- editor/src/views/object_tree.js
+++ editor/src/views/object_tree.js
@@ -11,30 +11,6 @@ function createNestingLines(depth) {
   return els
 }
 
-function handleComponentNodeClick(app, component, evt) {
-  
-}
-
-function createComponentBranchDivs(app, component, depth=0) {
-  let caret = component.children.length ? [e('span', {className: "component-caret-down"})] : []
-  let div = e('div', {className: "component-node", onclick: (evt) => handleComponentNodeClick(app, component, evt)}, [
-    ...caret,
-    ...createNestingLines(depth),
-    e('span', {className: "component-icon", style: `background-image: url('${component.getIconUrl()}')`})
-  ])
-  if (component.isTextBlock || component.isCodeBlock) {
-    div.appendChild(e('span', {className: "component-quote"}, [component.getQuote()]))
-  } else {
-    div.appendChild(e('span', {className: "component-label"}, [component.getLabel()]))
-    div.appendChild(e('span', {className: "component-description"}, [component.getDescription()||""]))
-  }
-  let divs = [div]
-  component.children.forEach(c => {
-    divs = [...divs, ...createComponentBranchDivs(app, c, depth+1)]
-  })
-  return divs
-}
-
 class ObjectTree extends DockView {
 
   static itemId = "obj_tree"
@@ -54,7 +30,7 @@ class ObjectTree extends DockView {
 
     if (this.doc) {
       let roots = this.doc.getRootComponents()
-      tree.replaceChildren(...roots.map(r => createComponentBranchDivs(this.app, r)).flat())
+      tree.replaceChildren(...roots.map(r => this.createComponentBranchDivs(r)).flat())
     }
 
     ref.appendChild(tree)
@@ -63,6 +39,34 @@ class ObjectTree extends DockView {
   onDocumentBatchChange(doc) {
     this.doc = doc
     if (this.isActive()) {this.render()}
+  }
+
+  handleComponentNodeClick(component, evt) {
+    if (component.children) {
+      //let state = this.app.getCurrentFileState()
+      //state.update()
+      // this.objectsExpanded[path] = !this.objectsExpanded[path]
+    }
+  }
+  
+  createComponentBranchDivs(component, depth=0) {
+    let caret = component.children.length ? [e('span', {className: "component-caret-down"})] : []
+    let div = e('div', {className: "component-node", onclick: (evt) => this.handleComponentNodeClick(component, evt)}, [
+      ...caret,
+      ...createNestingLines(depth),
+      e('span', {className: "component-icon", style: `background-image: url('${component.getIconUrl()}')`})
+    ])
+    if (component.isTextBlock || component.isCodeBlock) {
+      div.appendChild(e('span', {className: "component-quote"}, [component.getQuote()]))
+    } else {
+      div.appendChild(e('span', {className: "component-label"}, [component.getLabel()]))
+      div.appendChild(e('span', {className: "component-description"}, [component.getDescription()||""]))
+    }
+    let divs = [div]
+    component.children.forEach(c => {
+      divs = [...divs, ...this.createComponentBranchDivs(c, depth+1)]
+    })
+    return divs
   }
 
 }
