33b2c9c3425eb67f5a3574d0c0db31df1a092a38
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-17 15:14:59 -0400

    .

--- editor/src/models/selection.js
+++ editor/src/models/selection.js
@@ -27,6 +27,50 @@ export const SELECTION_TYPE = {
 
 // }
 
+export const SelectionV2 = {
+
+  selectNode(node) {
+    return { isNode: true, node }
+  },
+  
+  selectNodeList(nodeList) {
+    return { isNodeList: true, nodeList }
+  },
+  
+  selectText(todo) {
+    return {}
+  },
+  
+  selectFile(file) {
+    return { isFile: true, file }
+  },
+  
+  selectFileList(fileList) {
+    return { isFileList: true, fileList }
+  },
+
+  merge(first, second) {
+
+    // Group nodes together
+    if ((first.isNode || first.isNodeList) && (second.isNode || second.isNodeList)) {
+      let a = first.isNode ? [first.node] : first.nodeList
+      let b = second.isNode ? [second.node] : second.nodeList
+      return SelectionV2.selectNodeList([...a, ...b])
+    }
+
+    // Group files together
+    if ((first.isFile || first.isFileList) && (second.isFile || second.isFileList)) {
+      let a = first.isFile ? [first.file] : first.fileList
+      let b = second.isFile ? [second.file] : second.fileList
+      return SelectionV2.selectFileList([...a, ...b])
+    }
+
+    // Otherwise, they can't be merged, return the second selection
+    return second
+  }
+  
+}
+
 export class Selection {
 
   // Source of change: Does it come from clicking in the side menu? From the text editor?
