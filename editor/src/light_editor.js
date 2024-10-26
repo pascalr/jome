fadd2967f4900e5952e293486cce987019a07ccf
Pascal Rainville <pascal.rainville@gmail.com>
2024-10-26 07:40:29 -0400

    .

--- editor/src/light_editor.js
+++ editor/src/light_editor.js
@@ -3,18 +3,17 @@ import { NeutralinoApp } from './neutralino_app'
 import { Canvas } from './renderers/Canvas';
 import { Field } from './components/Field';
 import { Calc } from './components/Calc';
-import { Rect } from './components/drawing/Rect';
 import { Line } from './components/drawing/Line';
 import { Isogon } from './components/drawing/Isogon';
 
-import { Rect as PrimitiveRect } from './components/primitives/rect';
-import { Text } from './components/primitives/text';
+import { Rect } from './components/primitives/rect';
+import { Text } from './components/primitives/txt';
 
 document.addEventListener('DOMContentLoaded', async () => {
 
   let app = new NeutralinoApp()
-  app.registerComponents([Canvas, Field, Calc, Rect, Line, Isogon])
-  app.registerPrimitives([PrimitiveRect, Text])
+  app.registerComponents([Canvas, Field, Calc, Line, Isogon])
+  app.registerPrimitives([Rect, Text])
 
   await app.setup(document.getElementById('root')) // TODO: Rename this
 });
